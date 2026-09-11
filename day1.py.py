c1f0f7dc56fd4effe0197fb217cb2807bc35d0dc
mohanakrishna77 <mohan.chiru77@gmail.com>
2026-09-11 16:03:54 +0530

    Add files via upload

---
+++ day1.py.py
@@ -0,0 +1,17 @@
+
+print('Hello QA Manager')
+print(" I am restarting my career from scratch today ")
+
+name = 'Mohana Krishna'
+current_role = 'QA Manager'
+targer_role = 'Senior QA Manager'
+expected_salary = 65000000
+skill = "Python"
+
+print ("My Name is ", name)
+print ("My current role is", current_role)
+print ("Now my targer role", targer_role)
+print ('Skill I am learning now is ', skill)
+print ('My Targeted salary is ', expected_salary)
+
+
